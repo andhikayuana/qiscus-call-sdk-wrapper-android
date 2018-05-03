f902e919bb4e2e550461dbf0893d5ae786fff8e7
Zetra <zetra@mail.ugm.ac.id>
2018-05-03 11:22:29 +0700

    Initial commit

---
+++ app/src/main/java/com/qiscus/sdk/call/agora/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.qiscus.sdk.call.agora.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
