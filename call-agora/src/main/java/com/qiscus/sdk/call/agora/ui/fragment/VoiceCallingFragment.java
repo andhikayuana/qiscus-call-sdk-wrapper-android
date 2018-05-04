843af6c9b14102bd74e9fd51c9a0f5b1a3888bb1
Zetra <zetra@mail.ugm.ac.id>
2018-05-04 16:01:57 +0700

    Add calling fragment

---
+++ call-agora/src/main/java/com/qiscus/sdk/call/agora/ui/fragment/VoiceCallingFragment.java
@@ -0,0 +1,14 @@
+package com.qiscus.sdk.call.agora.ui.fragment;
+
+import com.qiscus.sdk.call.agora.R;
+
+/**
+ * Created by rahardyan on 06/06/17.
+ */
+
+public class VoiceCallingFragment extends CallingFragment {
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_qiscus_calling_voice;
+    }
+}
