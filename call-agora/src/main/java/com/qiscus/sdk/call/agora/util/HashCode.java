321f0174d1f3dbe41ed7345900a5d9920aaff637
Zetra <zetra@mail.ugm.ac.id>
2018-05-04 15:58:08 +0700

    Add hashcode util

---
+++ call-agora/src/main/java/com/qiscus/sdk/call/agora/util/HashCode.java
@@ -0,0 +1,19 @@
+package com.qiscus.sdk.call.agora.util;
+
+import java.util.Arrays;
+
+/**
+ * Created on : May 04, 2018
+ * Author     : zetbaitsu
+ * Name       : Zetra
+ * GitHub     : https://github.com/zetbaitsu
+ */
+public final class HashCode {
+    private HashCode() {
+
+    }
+
+    public static int hash(Object... values) {
+        return Arrays.hashCode(values);
+    }
+}
